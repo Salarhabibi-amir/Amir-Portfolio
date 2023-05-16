1c323e51c89780a65b9aae2be57dbb5044731d2f
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-05-16 17:02:41 +0430

    Fix eslint errores.

--- javascript.js
+++ javascript.js
@@ -2,21 +2,19 @@ const portfoliobtn = document.querySelector('.close-overlap1');
 const aboutbtn = document.querySelector('.close-overlap2');
 const contactbtn = document.querySelector('.close-overlap3');
 const barbtn = document.querySelector('.fa-bars');
-const closebtn = document.querySelector('.closebtn')
-closebtn.addEventListener('click', function() {
-    document.getElementById("myNav").style.height = "0%";
-
-})
-barbtn.addEventListener('click', function() {
-    document.getElementById("myNav").style.height = "100%";
-
-})
-portfoliobtn.addEventListener('click', function() {
-    document.getElementById("myNav").style.height = "0%";
-})
-aboutbtn.addEventListener('click', function() {
-    document.getElementById("myNav").style.height = "0%";
-})
-contactbtn.addEventListener('click', function() {
-    document.getElementById("myNav").style.height = "0%";
-})
+const closebtn = document.querySelector('.closebtn');
+closebtn.addEventListener('click', () => {
+  document.getElementById('myNav').style.height = '0%';
+});
+barbtn.addEventListener('click', () => {
+  document.getElementById('myNav').style.height = '100%';
+});
+portfoliobtn.addEventListener('click', () => {
+  document.getElementById('myNav').style.height = '0%';
+});
+aboutbtn.addEventListener('click', () => {
+  document.getElementById('myNav').style.height = '0%';
+});
+contactbtn.addEventListener('click', () => {
+  document.getElementById('myNav').style.height = '0%';
+});
