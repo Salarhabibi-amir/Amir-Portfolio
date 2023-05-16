94bae4d761a24dd5825624ddc380f8bede1195f0
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-05-16 16:28:19 +0430

    Use javascript best paractice

--- javascript.js
+++ javascript.js
@@ -1,15 +1,16 @@
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-
-}
 let portfoliobtn = document.querySelector('.close-overlap1');
 let aboutbtn = document.querySelector('.close-overlap2');
 let contactbtn = document.querySelector('.close-overlap3');
+let barbtn = document.querySelector('.fa-bars');
+let closebtn = document.querySelector('.closebtn')
+closebtn.addEventListener('click', function() {
+    document.getElementById("myNav").style.height = "0%";
 
+})
+barbtn.addEventListener('click', function() {
+    document.getElementById("myNav").style.height = "100%";
+
+})
 portfoliobtn.addEventListener('click', function() {
     document.getElementById("myNav").style.height = "0%";
 })
