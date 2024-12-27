e41d0fdc7bae722af721779c538defafc775cfb9
DESKTOP-6APCCDP\Habibi <a.hussainhabibi@gmail.com>
2024-12-28 00:22:02 +0430

    Add links of each projects.

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -3,10 +3,12 @@
         <h1 class="text-xl">My Projects</h1>
 
         <div class="flex flex-col items-center justify-center p-4 w-100 border text-lg ">
-            <div class="bg-green-500 border w-full text-center rounded p-6">🏢 HRIMS and Employee Portal</div>            
-            <div class="bg-red-500 border w-full text-center rounded p-6">🧑‍💻 Inventory and Asset Management (Darayee)</div>
-            <div class="bg-blue-500 border w-full text-center rounded p-6">🏘️ Emllak</div>
-            <div class="bg-yellow-500 border w-full text-center rounded p-6">🚘 Karwwan</div>
+            <a href="http://alhawaims.com" class="bg-green-500 border w-full text-center rounded p-6" target="_blank">🏢 HRIMS and Employee Portal</a>            
+            <a href="http://daraaye.com" class="bg-red-500 border w-full text-center rounded p-6" target="_blank">🧑‍💻 Inventory and Asset Management (Darayee)</a>
+            <a href="http://emllak.com" class="bg-blue-500 border w-full text-center rounded p-6" target="_blank">🏘️ Emllak</a>
+            <a href="http://karwwan.com" class="bg-yellow-500 border w-full text-center rounded p-6" target="_blank">🚘 Karwwan</a>
+            <a href="http://alhawatechs.com" class="bg-yellow-500 border w-full text-center rounded p-6" target="_blank"> Al-Hawa Technology website</a>
+            <a href="http://alhawagc.com" class="bg-yellow-500 border w-full text-center rounded p-6" target="_blank"> Al-Hawa Group of companies website</a>
         </div>
     </div>
 </template>
