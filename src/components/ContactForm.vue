957ae6f2148ed3073011b32a775a8a78e4fa22b0
DESKTOP-6APCCDP\Habibi <a.hussainhabibi@gmail.com>
2025-01-06 21:50:15 +0430

    change the headline design

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -2,7 +2,7 @@
     <div class="mx-w-lg p-6 container mx-auto">
         <div class="">
             <h1>
-                Coming soo..
+                Coming soon..
             </h1>
         </div>
     </div>
--- src/components/HeadLine.vue
+++ src/components/HeadLine.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 p-6">
+    <div class="flex flex-col gap-8 p-6">
         <div class="left-column">
             <h4 class="text-2xl font-bold mb-4">Full Stack Developer</h4>
             <p class="mb-4">
-                I am Amir Hussain Habibi.I got my bachelor degree in Computer Science in 2022 from KPU( Kabul
-                Polytechnic University). I am a Full Stack Web Developer with more than three years of experience in
-                designing and
-                developing different websites, Web applications, and databases.
+                My name is Amir Hussain Habibi, and I hold a Bachelor's degree in Computer Science from Kabul
+                Polytechnic University, which I completed in 2022. As a Full Stack Web Developer with over three years
+                of hands-on experience, I specialize in designing and developing a variety of websites and web
+                applications. My primary stack includes Laravel and Vue.js, and I also have expertise in CodeIgniter,
+                Firebase, React, and WordPress. I am passionate about creating efficient, user-friendly solutions that
+                meet the unique needs of each project.
             </p>
             <p>
-                I have worked on different type of web applications, such us <a href="http://emllak.com" target="_blank"
+                I have worked on various types of web applications, such  <a href="http://emllak.com" target="_blank"
                     class="text-blue-600 hover:underline">Emllak</a>, <a href="http://karwwan.com"
                     class="text-blue-600 hover:underline" target="_blank">Karwwan</a>, <a href="http://daraaye.com"
                     class="text-blue-600 hover:underline">Darayee</a> , <a href="http://alhawaims.com" target="_blank"
-                    class="text-blue-600 hover:underline">HRIMS</a> platforms,  <a
-                    href="http://alhawatechs.com" class="text-blue-600 hover:underline" target="_blank">Al-Hawa
+                    class="text-blue-600 hover:underline">HRIMS</a> platforms, <a href="http://alhawatechs.com"
+                    class="text-blue-600 hover:underline" target="_blank">Al-Hawa
                     Technologies</a> website and <a href="http://alhawagc.com" class="text-blue-600 hover:underline"
                     target="_blank">Al-Hawa Group of Companies</a> website.
             </p>
