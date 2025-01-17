be40093d723a4e5673c31e1a1f49cd2528039d9b
DESKTOP-6APCCDP\Habibi <a.hussainhabibi@gmail.com>
2025-01-17 14:21:37 +0430

    remove vite.

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
-export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/Amir-Portfolio/" : "/",
-  plugins: [vue()],
-});
