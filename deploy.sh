142db49546bcf99ab5930bf6441e22b5e08cf01b
DESKTOP-6APCCDP\Habibi <a.hussainhabibi@gmail.com>
2026-03-05 00:15:14 +0430

    .

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# use main instead of master if your default branch is main
-git push -f git@github.com:Salarhabibi-amir/Amir-Portfolio.git main:gh-pages
+# use current branch (main or master) in dist to push to gh-pages
+git push -f git@github.com:Salarhabibi-amir/Amir-Portfolio.git "$(git branch --show-current):gh-pages"
 
 cd -
